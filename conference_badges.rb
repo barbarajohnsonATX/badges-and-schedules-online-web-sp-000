6f042f714b50e2ed546d73cc01beafc495e2f3f7
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-05 00:23:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,8 @@ end
 
 def batch_badge_creator(names)
   messages = Array.new 
-  room = 1 
   names.each do |name|
     messages.push("Hello, my name is #{name}.")
-    room += 1
   end 
   messages 
 end 
